4824d89bcf1afb90678c8785fd1514d9e42825e3
Ousmane DIOUF <68850373+ousmanediouf88@users.noreply.github.com>
2021-10-29 23:25:53 +0200

    Update check_environment.py

--- check_environment.py
+++ check_environment.py
@@ -1,3 +1,23 @@
 import importlib
 
-packages = ['geopandas', 'contextily', 'fiona', 'rtree', 'mapclassify', 'PIL']
+packages = ['geopandas', 'contextily', 'fiona', 'rtree', 'mapclassify']
+
+bad = []
+for package in packages:
+    try:
+        importlib.import_module(package)
+    except ImportError:
+        bad.append("Can't import %s" % package)
+else:
+    if len(bad) > 0:
+        print('Your tutorial environment is not yet fully set up:')
+        print('\n'.join(bad))
+    else:
+        try:
+            import geopandas
+            countries = geopandas.read_file("data/LOC_SN.shp")
+            print("All good. Enjoy the tutorial!")
+        except Exception as e:
+            print("Couldn't read countries shapefile.")
+            print(e)
+
